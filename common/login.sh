9144c7cfeab25fe09bf76c47842fc392ac96a54b
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2022-06-11 22:27:12 +0900

    update

--- common/login.sh
+++ common/login.sh
@@ -4,3 +4,6 @@
 
 # 入力モードを「直接入力」にする
 ibus engine 'xkb:us::eng'
+
+# ワークスペース名設定
+xfconf-query -c xfwm4 -p /general/workspace_names -t string -t string -t string -t string -t string -t string -t string -t string -t string -t string -s "1" -s "2" -s "3" -s "4" -s "5" -s "6" -s "7" -s "8" -s "9" -s "0"
